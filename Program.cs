a2d8a4910d18079a96754415cef611d393ee9d03
Mikhail014 <msemin704@gmail.com>
2022-09-14 07:39:07 +0300

    Задача с гирями

--- Program.cs
+++ Program.cs
@@ -1,13 +1,39 @@
-﻿Console.Write("Enter username: ");
-string username = Console.ReadLine();
+﻿int a = 1;
+int b = 2;
+int c = 6;
+int d = 8;
+int e = 4;
 
-if (username.ToLower() == "masha"){
-    Console.WriteLine("Hooray! it's Masha");
-}
-else{
-    Console.Write("Hello, ");
-    Console.WriteLine(username);
-}
+int max = a;
+
+if (a > max) max = a;
+if (b > max) max = b;
+if (c > max) max = c;
+if (d > max) max = d;
+if (e > max) max = e;
+
+Console.Write("max = ");
+Console.WriteLine(max);
+
+
+
+
+
+
+
+
+
+
+// Console.Write("Enter username: ");
+// string username = Console.ReadLine();
+
+// if (username.ToLower() == "masha"){
+//     Console.WriteLine("Hooray! it's Masha");
+// }
+// else{
+//     Console.Write("Hello, ");
+//     Console.WriteLine(username);
+// }
 
 
 
