8cd84977929b9bad9134f46f622a83386de5a8f8
Mikhail014 <msemin704@gmail.com>
2022-09-14 07:20:55 +0300

    Деление двух чисел

--- Program.cs
+++ Program.cs
@@ -1,6 +1,14 @@
-﻿int num1 = 3;
-int num2 = 5;
-Console.WriteLine(num1 + num2);
+﻿double num1 = 12;
+double num2 = 5;
+Console.WriteLine(num1 / num2);
+
+
+
+
+
+// int num1 = 3;
+// int num2 = 5;
+// Console.WriteLine(num1 + num2);
 
 
 
