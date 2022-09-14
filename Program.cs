3da7856bc041ba7005843ceb4d69a6b13136b947
Mikhail014 <msemin704@gmail.com>
2022-09-14 07:16:41 +0300

    Сложение двух чисел

--- Program.cs
+++ Program.cs
@@ -1,4 +1,10 @@
-﻿Console.Write("Введите Ваше имя: ");
-string username = Console.ReadLine();
-Console.Write("Привет, ");
-Console.Write(username);
+﻿int num1 = 3;
+int num2 = 5;
+Console.WriteLine(num1 + num2);
+
+
+
+// Console.Write("Введите Ваше имя: ");
+// string username = Console.ReadLine();
+// Console.Write("Привет, ");
+// Console.Write(username);
