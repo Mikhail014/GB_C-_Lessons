685963a64c035bad39501ddff64ee0af4a4d3866
Mikhail014 <msemin704@gmail.com>
2022-09-14 07:25:58 +0300

    Случайные числа

--- Program.cs
+++ Program.cs
@@ -1,6 +1,6 @@
-﻿double num1 = 12;
-double num2 = 5;
-Console.WriteLine(num1 / num2);
+﻿double num1 = new Random().Next(1, 10);
+double num2 = new Random().Next(1, 10);
+Console.WriteLine(num1 + num2);
 
 
 
