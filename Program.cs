eeb161cd1d08526bf168479d1a94df34c6a26db4
Mikhail014 <msemin704@gmail.com>
2022-09-14 07:34:43 +0300

    Условные конструкции

--- Program.cs
+++ Program.cs
@@ -1,6 +1,19 @@
-﻿double num1 = new Random().Next(1, 10);
-double num2 = new Random().Next(1, 10);
-Console.WriteLine(num1 + num2);
+﻿Console.Write("Enter username: ");
+string username = Console.ReadLine();
+
+if (username.ToLower() == "masha"){
+    Console.WriteLine("Hooray! it's Masha");
+}
+else{
+    Console.Write("Hello, ");
+    Console.WriteLine(username);
+}
+
+
+
+// double num1 = new Random().Next(1, 10);
+// double num2 = new Random().Next(1, 10);
+// Console.WriteLine(num1 + num2);
 
 
 
